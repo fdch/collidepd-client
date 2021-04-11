e5fa4a207220fed2f232d4a65a021b34bd1156cc
fd <fd@fdch.fibertel.com.ar>
2021-04-10 20:35:08 -0400

    update client

--- client.js
+++ client.js
@@ -272,7 +272,6 @@ if (LOCALSERVER==1) {
 
   // homepage
 
-
   app.use(express.static(path.join(__dirname, 'gui')));
 
   // serve the homepage
@@ -280,7 +279,7 @@ if (LOCALSERVER==1) {
     res.sendFile(__dirname + '/index.html');
   });
 
-
+  var PORT = 5011;
   var isUseHTTPs = true;
 
   const jsonPath = {
@@ -298,192 +297,12 @@ if (LOCALSERVER==1) {
 
   RTCPORT = config.port;
 
-  if(isUseHTTPs === false) {
-      isUseHTTPs = config.isUseHTTPs;
+  if(PORT === 5011) {
+    PORT = config.port;
   }
 
-  function serverHandler(request, response) {
-      // to make sure we always get valid info from json file
-      // even if external codes are overriding it
-      config = getValuesFromConfigJson(jsonPath);
-      config = getBashParameters(config, BASH_COLORS_HELPER);
-
-      // HTTP_GET handling code goes below
-      try {
-          var uri, filename;
-
-          try {
-              if (!config.dirPath || !config.dirPath.length) {
-                  config.dirPath = null;
-              }
-
-              uri = url.parse(request.url).pathname;
-              filename = path.join(config.dirPath ? resolveURL(config.dirPath) : process.cwd(), uri);
-          } catch (e) {
-              pushLogs(config, 'url.parse', e);
-          }
-
-          filename = (filename || '').toString();
-
-          if (request.method !== 'GET' || uri.indexOf('..') !== -1) {
-              try {
-                  response.writeHead(401, {
-                      'Content-Type': 'text/plain'
-                  });
-                  response.write('401 Unauthorized: ' + path.join('/', uri) + '\n');
-                  response.end();
-                  return;
-              } catch (e) {
-                  pushLogs(config, '!GET or ..', e);
-              }
-          }
-
-          if(filename.indexOf(resolveURL('/admin/')) !== -1 && config.enableAdmin !== true) {
-              try {
-                  response.writeHead(401, {
-                      'Content-Type': 'text/plain'
-                  });
-                  response.write('401 Unauthorized: ' + path.join('/', uri) + '\n');
-                  response.end();
-                  return;
-              } catch (e) {
-                  pushLogs(config, '!GET or ..', e);
-              }
-              return;
-          }
-
-          var matched = false;
-          ['/demos/', '/dev/', '/dist/', '/socket.io/', '/node_modules/canvas-designer/', '/admin/'].forEach(function(item) {
-              if (filename.indexOf(resolveURL(item)) !== -1) {
-                  matched = true;
-              }
-          });
-
-          // files from node_modules
-          ['RecordRTC.js', 'FileBufferReader.js', 'getStats.js', 'getScreenId.js', 'adapter.js', 'MultiStreamsMixer.js'].forEach(function(item) {
-              if (filename.indexOf(resolveURL('/node_modules/')) !== -1 && filename.indexOf(resolveURL(item)) !== -1) {
-                  matched = true;
-              }
-          });
-
-          if (filename.search(/.js|.json/g) !== -1 && !matched) {
-              try {
-                  response.writeHead(404, {
-                      'Content-Type': 'text/plain'
-                  });
-                  response.write('404 Not Found: ' + path.join('/', uri) + '\n');
-                  response.end();
-                  return;
-              } catch (e) {
-                  pushLogs(config, '404 Not Found', e);
-              }
-          }
-
-          ['Video-Broadcasting', 'Screen-Sharing', 'Switch-Cameras'].forEach(function(fname) {
-              try {
-                  if (filename.indexOf(fname + '.html') !== -1) {
-                      filename = filename.replace(fname + '.html', fname.toLowerCase() + '.html');
-                  }
-              } catch (e) {
-                  pushLogs(config, 'forEach', e);
-              }
-          });
-
-          var stats;
-
-          try {
-              stats = fs.lstatSync(filename);
-
-              if (filename.search(/demos/g) === -1 && filename.search(/admin/g) === -1 && stats.isDirectory() && config.homePage === '/demos/index.html') {
-                  if (response.redirect) {
-                      response.redirect('/demos/');
-                  } else {
-                      response.writeHead(301, {
-                          'Location': '/demos/'
-                      });
-                  }
-                  response.end();
-                  return;
-              }
-          } catch (e) {
-              response.writeHead(404, {
-                  'Content-Type': 'text/plain'
-              });
-              response.write('404 Not Found: ' + path.join('/', uri) + '\n');
-              response.end();
-              return;
-          }
-
-          try {
-              if (fs.statSync(filename).isDirectory()) {
-                  response.writeHead(404, {
-                      'Content-Type': 'text/html'
-                  });
-
-                  if (filename.indexOf(resolveURL('/demos/MultiRTC/')) !== -1) {
-                      filename = filename.replace(resolveURL('/demos/MultiRTC/'), '');
-                      filename += resolveURL('/demos/MultiRTC/index.html');
-                  } else if (filename.indexOf(resolveURL('/admin/')) !== -1) {
-                      filename = filename.replace(resolveURL('/admin/'), '');
-                      filename += resolveURL('/admin/index.html');
-                  } else if (filename.indexOf(resolveURL('/demos/dashboard/')) !== -1) {
-                      filename = filename.replace(resolveURL('/demos/dashboard/'), '');
-                      filename += resolveURL('/demos/dashboard/index.html');
-                  } else if (filename.indexOf(resolveURL('/demos/video-conference/')) !== -1) {
-                      filename = filename.replace(resolveURL('/demos/video-conference/'), '');
-                      filename += resolveURL('/demos/video-conference/index.html');
-                  } else if (filename.indexOf(resolveURL('/demos')) !== -1) {
-                      filename = filename.replace(resolveURL('/demos/'), '');
-                      filename = filename.replace(resolveURL('/demos'), '');
-                      filename += resolveURL('/demos/index.html');
-                  } else {
-                      filename += resolveURL(config.homePage);
-                  }
-              }
-          } catch (e) {
-              pushLogs(config, 'statSync.isDirectory', e);
-          }
-
-          var contentType = 'text/plain';
-          if (filename.toLowerCase().indexOf('.html') !== -1) {
-              contentType = 'text/html';
-          }
-          if (filename.toLowerCase().indexOf('.css') !== -1) {
-              contentType = 'text/css';
-          }
-          if (filename.toLowerCase().indexOf('.png') !== -1) {
-              contentType = 'image/png';
-          }
-
-          fs.readFile(filename, 'binary', function(err, file) {
-              if (err) {
-                  response.writeHead(500, {
-                      'Content-Type': 'text/plain'
-                  });
-                  response.write('404 Not Found: ' + path.join('/', uri) + '\n');
-                  response.end();
-                  return;
-              }
-
-              try {
-                  file = file.replace('connection.socketURL = \'/\';', 'connection.socketURL = \'' + config.socketURL + '\';');
-              } catch (e) {}
-
-              response.writeHead(200, {
-                  'Content-Type': contentType
-              });
-              response.write(file, 'binary');
-              response.end();
-          });
-      } catch (e) {
-          pushLogs(config, 'Unexpected', e);
-
-          response.writeHead(404, {
-              'Content-Type': 'text/plain'
-          });
-          response.write('404 Not Found: Unexpected error.\n' + e.message + '\n\n' + e.stack);
-          response.end();
-      }
+  if(isUseHTTPs === false) {
+      isUseHTTPs = config.isUseHTTPs;
   }
 
   var httpApp;
@@ -527,7 +346,6 @@ if (LOCALSERVER==1) {
               pfx: sslKey
           };
       }
-
       httpApp = httpServer.createServer(options, serverHandler);
   } else {
       httpApp = httpServer.createServer(serverHandler);
@@ -535,12 +353,10 @@ if (LOCALSERVER==1) {
 
   
   RTCMultiConnectionServer.beforeHttpListen(httpApp, config);
+
   httpApp = httpApp.listen(process.env.PORT || PORT, process.env.IP || "0.0.0.0", function() {
       RTCMultiConnectionServer.afterHttpListen(httpApp, config);
   });
-
-
-  // server.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
   ioServer(httpApp).on('connection', function(socket) {
       RTCMultiConnectionServer.addSocket(socket, config);
